33af19da1a8f9eed106db46cd3f59068949fa992
Jannchie <jannchie@gmail.com>
2022-01-13 00:15:10 +0900

    feat: teset

--- speedo_test.go
+++ speedo_test.go
@@ -1,13 +1,12 @@
 package speedo
 
 import (
-	"log"
 	"testing"
 	"time"
 )
 
 func TestSpeedoWithServer(t *testing.T) {
-	s := NewSpeedometer(Config{Log: true, Server: "http://:8080", PostIntervalSEC: 5})
+	s := NewSpeedometer(Config{Log: true, Server: "http://:80", PostIntervalSEC: 5})
 	ticker := time.NewTicker(time.Second)
 	count := 20
 	for i := 0; i < count; i++ {
@@ -20,13 +19,12 @@ func TestSpeedoWithServer(t *testing.T) {
 }
 
 func TestProgressSpeedo(t *testing.T) {
-	s := NewProgressSpeedometer(15, Config{Log: true, PrintIntervalSEC: 1})
+	s := NewProgressSpeedometer(15, Config{Log: true, Server: "http://:80", PrintIntervalSEC: 1, PostIntervalSEC: 1})
 	ticker := time.NewTicker(time.Second / 10)
 	count := 50
 	for i := 0; i < count; i++ {
 		<-ticker.C
 		s.AddCount(1)
-		log.Println(s.GetStatusString())
 	}
 	if s.count != uint64(count) {
 		t.Error("not equal")
@@ -34,7 +32,7 @@ func TestProgressSpeedo(t *testing.T) {
 }
 
 func TestVariationSpeedo(t *testing.T) {
-	s := NewVariationSpeedometer(Config{Log: true, PrintIntervalSEC: 1})
+	s := NewVariationSpeedometer(Config{Log: true, Server: "http://:80", PrintIntervalSEC: 1, PostIntervalSEC: 1})
 	ticker := time.NewTicker(time.Second / 100)
 	count := 1000
 	for i := 0; i < count; i++ {
